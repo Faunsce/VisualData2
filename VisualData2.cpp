f81a1abaaebc550cb1ac3e46ddedcd1c0b3cbcae
Dominik Rutter <dominik.rutter@gmail.com>
2023-10-09 00:17:56 -0400

    Add project files.

---
+++ VisualData2.cpp
@@ -0,0 +1,52 @@
+﻿// VisualData2.cpp : Defines the entry point for the application.
+//
+
+#include "VisualData2.h"
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+	const int SIZE_OF_DATA = 10;
+	std::vector<int> data(SIZE_OF_DATA);
+	for (int i = 0; i < SIZE_OF_DATA; i++)
+	{
+		data[i] = i;
+	}
+
+	std::cout << "Your data Madame~" << std::endl
+		<< [data]() {std::string msg; for (auto& num : data) { msg += std::to_string(num) + " "; } return msg; }() << std::endl << std::endl;
+
+	std::random_device randomDevice;
+	std::mt19937 randomEngine(randomDevice());
+	std::shuffle(data.begin(), data.end(), randomEngine);
+
+	std::cout << "Your data Madame~" << std::endl
+		<< [data]() {std::string msg; for (auto& num : data) { msg += std::to_string(num) + " "; } return msg; }() << std::endl << std::endl;
+	
+	bool clean = true;
+	for (int i = SIZE_OF_DATA - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (data[j] > data[j + 1])
+			{
+				clean = false;
+				std::swap(data[j], data[j + 1]);
+			}
+		}
+		if (clean)
+			break;
+		clean = true;
+	}
+
+	std::cout << "Your data Madame~" << std::endl
+		<< [data]() {std::string msg; for (auto& num : data) { msg += std::to_string(num) + " "; } return msg; }() << std::endl << std::endl;
+
+
+	return 0;
+}
